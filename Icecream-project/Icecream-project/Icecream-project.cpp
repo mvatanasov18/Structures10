ffe964ff016c9bab7a63eaf49606d663c0b82b38
mvatanasov18 <mvatanasov18@codingburgas.bg>
2020-04-16 19:50:52 +0300

    createOrder is ready for use

--- Icecream-project/Icecream-project/Icecream-project.cpp
+++ Icecream-project/Icecream-project/Icecream-project.cpp
@@ -1,19 +1,27 @@
 ﻿#include <iostream>
 using namespace std;
 
-struct PRODUCT {
-    string brand;
-    float price;
-    int quantity;
-    int expDate;
+struct PERSON {
+    string firstName="";
+    string lastName="";
+    int years=0;
 };
 
-void createOrder();
+struct PRODUCT {
+    string brand="";
+    string flavour="";
+    float price=0;
+    int quantity=0;
+    int expDate=0;
+    PERSON person;
+};
+
+void createOrder(PRODUCT* product, int& productCount);
 void showAvailProd();
 void clExProd();
 void deleteProduct();
 
-bool showMenu() {
+bool showMenu(PRODUCT* product,int& productCount) {
     int userInput;
     cout << "\nWelcome to our programme about icecream: " << endl;
     cout << "1. Create a new order" << endl;
@@ -26,7 +34,7 @@ bool showMenu() {
     switch (userInput)
     {
     case 1:
-        createOrder();
+        createOrder(PRODUCT * product, int& productCount);
         break;
     case 2:
         showAvailProd();
@@ -42,7 +50,30 @@ bool showMenu() {
     return true;
 }
 
-void createOrder() {
+void createOrder(PRODUCT* product, int& productCount) {
+    cout << "Enter brand: ";
+    cin >> product[productCount].brand;
+
+    cout << "Enter flavour: ";
+    cin >> product[productCount].flavour;
+
+    cout << "Enter price: ";
+    cin >> product[productCount].price;
+
+    cout << "Enter quantity: ";
+    cin >> product[productCount].quantity;
+
+    cout << "Enter expDate: ";
+    cin >> product[productCount].expDate;
+
+    cout << "Enter your firstname: ";
+    cin >> product[productCount].person.firstName;
+
+    cout << "Enter your lastname: ";
+    cin >> product[productCount].person.lastName;
+
+    cout << "Enter your years: ";
+    cin >> product[productCount].person.years;
 
 }
 
@@ -61,6 +92,6 @@ void deleteProduct() {
 int main()
 {
     PRODUCT product[100];
-    
-    while(showMenu());
+    int productCount = 0;
+    while(showMenu(product, productCount));
 }
